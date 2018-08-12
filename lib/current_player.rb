4a18746b985a23e8378b8fe6a6e19311b5217a0b
Sophie Gu <sophieqgu@gmail.com>
2018-08-12 14:29:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0 
-  board.each do |token|
-    if token == 'X' || token = 'O' 
+  board.each do |index|
+    if index == 'X' || index = 'O' 
       counter += 1 
     end
   end 
